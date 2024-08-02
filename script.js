9bf26e0ef25c6436089d849b87609f2417152fa3
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-02 07:46:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let angle = 0;
+const line = document.getElementById('line');
+
+function rotateLine() {
+    angle += 2;
+    line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+    requestAnimationFrame(rotateLine);
+}
+
+rotateLine();
